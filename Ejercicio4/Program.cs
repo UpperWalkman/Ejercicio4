c204fe61a3621cb0baa408b5aeb4b91c07cefcdb
Upper Walkman <UpperCIMC@outlook.com>
2023-08-13 16:17:39 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un caracter a contar");
+            string leerPirmeraLineea = Console.ReadLine();
+
+            Console.WriteLine("Escribe una serie de caracteres sin espacios");
+            string leerSerieCaracteres = Console.ReadLine();
+            char[] caracteres = leerSerieCaracteres.ToCharArray();
+            int totalRepetido = caracteres.Count(car => car == char.Parse(leerPirmeraLineea));
+
+            Console.WriteLine($"El caracteres '{leerPirmeraLineea}' son: {totalRepetido}");
+        }
+    }
+}
